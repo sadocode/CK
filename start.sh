d31f4fe7a6c8f9b418d3d5bf18132984a744ea19
sadocode <sadocode92@gmail.com>
2019-12-06 10:49:37 +0900

    [YCKE-2] completed.

--- start.sh
+++ start.sh
@@ -8,11 +8,22 @@ case $1 in
 start)
 	echo "Starting $SERVICE_NAME ..."
 	if [ ! -f $PID_PATH_NAME ]; then
-	 nohup java -jar $PATH_TO_JAR >> /dev/null &
-		echo $! > $PID_PATH_NAME
-	 echo "$SERVICE_NAME started ..."
-	else
-	 echo "$SERVICE_NAME is already running ..."
+	  if (( $# == 1 )); then
+	    nohup java -jar $PATH_TO_JAR >> /dev/null &
+    	  elif (( $# == 2 )); then
+	    nohup java -jar $PATH_TO_JAR $2 >> /dev/null &
+	  elif (( $# == 3 )); then
+	    nohup java -jar $PATH_TO_JAR $2 $3 >> /dev/null &
+    	  elif (( $# > 3 )); then
+	    echo "There are too many arguments! only 2 input will be entered."
+	    nohup java -jar $PATH_TO_JAR $2 $3 >> /dev/null &
+	  fi
+
+	  echo $! > $PID_PATH_NAME
+	  echo "$SERVICE_NAME started ..."
+	
+  	else
+	  echo "$SERVICE_NAME is already running ..."
 	fi
 ;;
 stop)
