0c15f19a3b25bba3a35699434ba5218f63ea4f3a
sadocode <sadocode92@gmail.com>
2019-12-09 07:17:50 +0000

    update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,9 @@ SERVICE_NAME=Yonhap_Article_Crawler
 PATH_TO_JAR=./crawler.jar
 PID_PATH_NAME=./Yonhap_Article_Crawler.pid
 
+TIME=300
+ARTICLE_SAVE_PATH=.
+
 case $1 in
 start)
 	echo "Starting $SERVICE_NAME ..."
@@ -37,4 +40,17 @@ stop)
 	 echo "$SERVICE_NAME is not running ..."
 	fi
 ;;
+info)
+	echo "크롤링 주기 = $TIME / 기사저장위치 = $ARTICLE_SAVE_PATH"
+;;
+help)
+	echo "--------------------------------------"
+	echo "실행 방법 : ./start.sh start [option]"
+	echo "실행 옵션 : 입력 X  --> 크롤링 주기 : 300, 기사 저장 위치 : ."
+	echo "		: time=xx --> 크롤링 주기 : xx초"
+	echo "		: path=yy --> 기사저장위치 : yy"
+	echo "종료 방법 : ./start.sh stop"
+	echo "프로그램 정보 확인 : ./start.sh info"
+	echo "--------------------------------------"
+;;	
 esac
