983079ac4d4f54c042cdb6ef5cf3e50f138dca18
sadocode <sadocode92@gmail.com>
2019-12-10 21:54:20 +0900

    [YCKE-2]

--- Crawler/src/main/java/com/hkj/Crawler/App.java
+++ Crawler/src/main/java/com/hkj/Crawler/App.java
@@ -1,49 +1,98 @@
 package com.hkj.Crawler;
 
+import java.util.regex.Pattern;
+
 public class App 
 {
 	private Crawler crawler;
 	private Thread thread;
 	
+	private static final String TIME = "^-time=[0-9]+$";
+	private static final String PATH = "-path=";
+	private int coolTime;
+	private String savePath;
+	
+	private Crawler initCrawler(String[] args)
+	{
+		Crawler temp = null;
+		
+		if(args.length == 0)
+			return temp = new Crawler();
+		
+		for(int i = 0; i < args.length; i++)
+		{
+			if(pathMatch(args[i]))
+			{
+				this.savePath = args[i].substring(6);
+				break;
+			}
+		}
+		for(int i = 0; i < args.length; i++)
+		{
+			if(timeMatch(args[i]))
+			{
+				try
+				{
+					this.coolTime = Integer.parseInt(args[i].substring(6));
+				}
+				catch(NumberFormatException nfe)
+				{
+					this.coolTime = 0;
+				}
+				break;
+			}
+		}	
+		
+		if(this.savePath != null & this.coolTime != 0)
+		{
+			try
+			{
+				temp = new Crawler(this.coolTime, this.savePath);
+			}
+			catch(IllegalArgumentException iae)
+			{
+				temp = new Crawler(this.savePath);
+			}
+		}
+		else if(this.savePath != null & this.coolTime == 0)
+		{
+			temp = new Crawler(this.savePath);
+		}
+		else if(this.savePath == null & this.coolTime != 0)
+		{
+			try
+			{
+				temp = new Crawler(this.coolTime);
+			}
+			catch(IllegalArgumentException iae)
+			{
+				temp = new Crawler();
+			}
+		}
+		else
+		{
+			temp = new Crawler();
+		}
+		return temp;
+	}
+	private boolean timeMatch(String data)
+	{
+		return Pattern.matches(TIME, data);
+	}
+	private boolean pathMatch(String data)
+	{
+		return data.startsWith(PATH);
+	}
     public static void main( String[] args )
     {
     	App app = new App();
-    	int temp = 0;
     	
-    	try
-    	{
-    		if(args.length == 0)
-    		{
-    			app.crawler = new Crawler();
-    		}
-    		else if(args.length == 1)
-    		{
-    			temp = Integer.parseInt(args[0]);
-    			app.crawler = new Crawler(temp);
-    		}
-    		else if(args.length == 2)
-    		{
-    			temp = Integer.parseInt(args[0]);
-    			app.crawler = new Crawler(temp, args[1]);
-    		}
-    		else
-    		{
-    			throw new java.lang.IllegalArgumentException("There are too many arguments");
-    		}
-    	}
-    	catch(NumberFormatException nfe)
-    	{
-    		app.crawler = new Crawler();
-    	}
-    	catch(IllegalArgumentException iae)
-    	{
-    		app.crawler = new Crawler();
-    	}
-    	finally
-    	{
-    		app.thread = new Thread(app.crawler);
-	    	app.thread.start();
-    	}
+    	app.crawler = app.initCrawler(args);
+    		
+    	
+    	app.thread = new Thread(app.crawler);
+    	app.thread.start();
+    	
     }
    
 }
